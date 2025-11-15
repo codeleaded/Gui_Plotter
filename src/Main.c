9ca83751eca1ca120b505e0ea30efeb8c57a3508
codeleaded <codeleaded@gmail.com>
2025-11-15 18:30:24 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -3,20 +3,24 @@
 
 
 Plotter p;
+Sprite plottsp;
 
 void Setup(AlxWindow* w){
 	p = Plotter_New((Rect){ {0.0f,0.0f},{1000.0f,1000.0f} });
+	Plotter_AddSet(&p,sinf,0.0f,F32_PI2,0.1f,RED,5);
 
-	Plotter_AddSet(&p,sinf,F32_PI2,RED,0);
+	plottsp = Sprite_New(1000,1000);
+	Plotter_Render(plottsp.img,plottsp.w,plottsp.h,&p);
 }
 void Update(AlxWindow* w){
 	
 	Clear(DARK_BLUE);
 
-	Plotter_Render(WINDOW_STD_ARGS,&p);
+	Sprite_Render(WINDOW_STD_ARGS,&plottsp,0.0f,0.0f);
 }
 void Delete(AlxWindow* w){
 	Plotter_Free(&p);
+	Sprite_Free(&plottsp);
 }
 
 int main(){
