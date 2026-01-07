05f7f34cc1fea77175822691a21b8e6f41e451fa
C-Reaper <gnu.c.reaper@gmail.com>
2026-01-07 23:33:25 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -7,9 +7,10 @@ Sprite plottsp;
 
 void Setup(AlxWindow* w){
 	p = Plotter_New((Rect){ {0.0f,0.0f},{1000.0f,1000.0f} },PLOTTER_ORIENT_UP);
-	Plotter_AddSet(&p,sinf,0.0f,F32_PI2,0.1f,GREEN,4);
+	//Plotter_AddSet(&p,sinf,0.0f,F32_PI2,0.1f,GREEN,4);
 	//Plotter_AddSet(&p,cosf,0.0f,2.0f * F32_PI2,0.1f,RED,0);
 	//Plotter_AddSet(&p,F32_Sigmoid,-2.0f * F32_PI2,2.0f * F32_PI2,0.1f,BLUE,0);
+	Plotter_LoadSet(&p,"./data/Dataset.txt",GREEN,4);
 
 	plottsp = Sprite_New(1000,1000);
 	Plotter_Render(plottsp.img,plottsp.w,plottsp.h,&p);
